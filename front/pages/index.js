ae516e9be57f5f00afcb189d4a13d16aee09f0c2
dojje <dojje@users.noreply.github.com>
2022-09-03 21:11:03 +0200

    added styles

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,38 @@
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return(
+  <main
+  style={{
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "var(--base-dark)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }}
+  >
+    <div
+    style={{
+      width: "50%",
+      backgroundColor: "var(--base)",
+    }}
+    >
+      <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+      >
+        <div>
+          join
+        </div>
+        <div>
+          start
+        </div>
+      </div>
+    </div>
+  </main>
+  )
 }
 
 export default HomePage
